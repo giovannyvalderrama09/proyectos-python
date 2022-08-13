7e0ca1505a8095f693db7b5af87191eb53c9e01a
209 <209@academicos.cesde.edu.co>
2022-08-13 13:10:02 -0500

    NTS3-se crea archivo con datos de entrada y de salida y se crea archivo con condicional

---
+++ main.py
@@ -0,0 +1,31 @@
+# Hola mundo con PYTHON
+
+
+# Salida por consola 
+""" print("Hola mundo con python")
+print('Hola mundo con python') """
+
+# Entradas 
+
+# Datos primitivos
+
+#Recibir datos por teclado
+
+cuidad=input("Digite la cuidad donde vives: ")
+print(f"la cuidad donde usted vive es: {cuidad}")
+
+
+#Recibir datos numericos por teclado
+
+numero1 = int(input("Digite un número: "))
+numero2 = int(input("Digite otro numero: "))
+
+print(f"el primer numero es: {numero1} y el segundo numero es: {numero2}")
+
+
+#Operadores aritmeticos
+
+
+
+suma = numero1 + numero2; 
+print(f"La suma de {numero1}" )
---
+++ reto1.py
@@ -0,0 +1,18 @@
+
+
+
+numero = int(input("Digite el numero:"))
+
+
+resultado = numero%3
+
+print(f"El residuo de {numero} es: {resultado}")
+
+#Condicional simple en python
+
+if(resultado == 0 ):    
+    print("Es multiple de 3")  
+else:
+    print("No es multiplo de 3")
+
+print("fin del programa")
